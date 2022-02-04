33cecfee2cccd4f4e2d4352d045fe1a3ae2caac9
Aniket Kumar <aniketkashyapak17@gmail.com>
2022-02-04 17:20:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+const mongoose=require('mongoose');
+const ejsMate=require('ejs-mate');
+const Catch = require('./utils/catch');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const { resolveSoa } = require('dns');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("views"));
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/chatroom',Catch((req,res,next)=>{
+    res.render('home');
+}))
+app.get('/chatroom/room',Catch((req,res,next)=>{
+    res.render('entertainment');
+}))
+app.get('/chatroom/room/chat',Catch((req,res,next)=>{
+    res.render('firebase');
+}));
+app.get('/chatroom/room/chat/:id',Catch((req,res,next)=>{
+    const {id}=req.params;
+    console.log(id);
+    res.render('firebase',{id});
+    
+}));
+app.get("/contact",(req,res)=>{
+    res.send("CONTACT PAGE")
+})
+app.post("/chatroom",(req,res)=>{
+    const {choice}=req.body;
+    res.redirect(`/chatroom/room#${choice}`);
+})
+app.post('/chatroom/room/chat',(req,res)=>{
+    const {custom}=req.body;
+    res.redirect(`/chatroom/room/chat/${custom}`);
+});
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log('Listening to port 8080');
+})
---
+++ views/assets/js/firebase.js
@@ -0,0 +1,268 @@
+let TITLE1 = document.querySelector('.title').innerHTML;
+let TITLE = TITLE1.toUpperCase();
+            
+document.querySelector('#topBar h2').innerHTML= TITLE;
+
+const Path = 'app/'+TITLE;
+
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js';
+import { getAuth, onAuthStateChanged, signInWithCustomToken,signInAnonymously  } from 'https://www.gstatic.com/firebasejs/9.6.5/firebase-auth.js';
+
+// import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { Picker } from 'https://cdn.jsdelivr.net/npm/emoji-picker-element@1.0.3/index.min.js';
+
+          
+            const picker = new Picker({
+                    locale: 'en',
+                    
+                    });
+
+                    
+             
+                    let emojiOn = false;
+                    document.querySelector('#emoji').addEventListener('click',()=>{
+                        if(emojiOn){
+                            
+                            picker.style.display='none';
+                            emojiOn=false;
+                             
+                        }else{
+                            
+                            picker.style.display='flex';
+                        emojiOn=true;
+                    }
+                        
+                    })
+                    document.querySelector('#chatContainer').appendChild(picker);
+                    picker.addEventListener('emoji-click', event => {
+                    const vvl = document.querySelector('#textInput');
+                     vvl.value = vvl.value+event.detail.unicode;
+                    });
+let username="";
+const firebaseConfig={
+// (insert your Firebase configuration here)
+apiKey: "AIzaSyB_5V33i1LF3KMfYfUYc33wBD5K0BmXSrk",
+authDomain: "basicexample-1dec4.firebaseapp.com",
+databaseURL: "https://basicexample-1dec4-default-rtdb.firebaseio.com",
+projectId: "basicexample-1dec4",
+storageBucket: "basicexample-1dec4.appspot.com",
+messagingSenderId: "64592502724",
+appId: "1:64592502724:web:e76cc90b36631dcbe74e91",
+measurementId: "G-F86RJZ7HHN"
+};
+
+
+
+const app =  initializeApp(firebaseConfig);
+
+
+import { getDatabase, ref, set, child, get, onValue, onChildAdded, push} from "https://www.gstatic.com/firebasejs/9.6.5/firebase-database.js";
+
+const db = getDatabase();
+const auth = getAuth();
+
+
+
+
+document.querySelector(".modal").classList.add("open");
+
+
+
+
+//when 'enter' is pressed when we are on input tag
+
+const input = document.querySelector("#textInput");
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.querySelector("#enterButton").click();
+    }
+});
+
+
+//Send the message
+document.querySelector("#enterButton").addEventListener('click',()=>{
+
+    const text = input.value;
+    
+    const debref = ref(db,Path);
+    const dbExact = push(debref)
+    set(dbExact, {
+        username: username,
+        message: text
+    });
+    input.value="";
+})
+
+
+//Read Message asynchonous method
+//READING for the first time and when the childs(messages) are added
+function addChilds(){
+    const starCountRef = ref(db, Path);
+    onChildAdded(starCountRef, (data) => {
+    console.log(data.val());
+    // const list = document.createElement('li');
+    // list.innerHTML=data.val().name + ": " + data.val().message;
+    // document.querySelector('ul').appendChild(list);
+
+    if(username==""){
+       
+     }else{ 
+         
+        if(data.val().username==username){
+        //outgoing message
+        const chatBubble = Object.assign(
+            document.createElement(`div`), 
+            { 
+                innerHTML: `       
+                <div class="incomingName" style="text-align:right">You</div>
+    <p>${data.val().message}</p>
+                      `,
+              classList: [`outgoingChat`], } );
+
+              document.querySelector('#chatBox').appendChild(chatBubble);
+    }else{
+        //incoming message
+        const chatBubble = Object.assign(
+            document.createElement(`div`), 
+            { 
+                innerHTML: `       
+                <div class="incomingName">${data.val().username}</div>
+    <p>${data.val().message}</p>
+                      `,
+              classList: [`incomingChat`], } );
+
+              document.querySelector('#chatBox').appendChild(chatBubble);
+    }
+    document.querySelector('#chatBox').scrollTop = document.querySelector('#chatBox').scrollHeight
+
+    }
+    
+    });
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+//LOGIN / SIGNUP METHOD
+document.querySelector('#moduleButton').addEventListener('click',()=>{
+
+
+username =  document.querySelector("#usernameInput").value.toLowerCase().trim();
+const pass = document.querySelector("#passwordInput").value.trim();
+// pass = pass.trim();
+// username=username.trim();
+if(username==""){
+    alert("username cannot be empty");
+    return;
+}
+//reading user if exist
+console.log(username);
+
+
+const dbRef = ref(getDatabase());
+get(child(dbRef, `users/${username}`)).then((snapshot) => {
+if (snapshot.exists()) {
+    //exist already, just login anmonymously..
+    if(snapshot.val().password==pass){
+        document.querySelector('#alertsOnly').innerHTML="";
+        const flash = Object.assign(
+            document.createElement(`div`), 
+            { 
+                innerHTML: `       
+               
+                <div class="alert alert-success" role="alert">
+Logging In!
+</div>
+                      `,
+              classList: [``], } );
+
+              document.querySelector('#alertsOnly').appendChild(flash);
+
+        signInAnonymously(auth)
+            .then(() => {
+                // Signed in..
+                document.querySelector(".modal").classList.remove("open");
+                console.log('Logging In');
+
+                ////del
+                addChilds();
+                ////del
+
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ...
+            });
+
+    }else{
+        console.log("Wrong Password");
+
+        const flash = Object.assign(
+            document.createElement(`div`), 
+            { 
+                innerHTML: `       
+               
+                <div class="alert alert-danger" role="alert">
+Wrong Password!
+</div>
+                      `,
+              classList: [``], } );
+
+              document.querySelector('#alertsOnly').appendChild(flash);
+    }
+ 
+
+} else {
+    
+    //Writing new username and logging in...
+        set(ref(db, 'users/'+username), {
+            password: pass
+        });
+
+        console.log("Writting new user");
+
+        
+        signInAnonymously(auth)
+            .then(() => {
+                // Signed in..
+                document.querySelector(".modal").classList.remove("open");
+                console.log('Logging In');
+                const flash = Object.assign(
+            document.createElement(`div`), 
+            { 
+                innerHTML: `       
+               
+                <div class="alert alert-success" role="alert">
+Logging In!
+</div>
+                      `,
+              classList: [``], } );
+              document.querySelector('#alertsOnly').appendChild(flash); 
+              addChilds();
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ...
+            });
+
+
+          
+
+}
+}).catch((error) => {
+console.error(error);
+});
+
+});
